339d8aa4527cdeb8e69572f3303edc29d9b080af
root <root@JenkinsTest1.(none)>
2016-06-15 06:57:37 -0400

    Gran Commit

--- aws_mgmt.sh
+++ aws_mgmt.sh
@@ -1,3 +1,9 @@
+function ec2describe()
+{
+ aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output text
+}
+
+ec2describe=`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output text > /tmp/intances`
 while :
 do
   clear
@@ -18,13 +24,16 @@ do
   case $choice in
   1)
     clear
-    running_intances_count=`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output text | grep running | wc -l`
+
+    # Getting the number of running instance and of intances
+    running_intances_count=`aws ec2 describe-instances --filter Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output text | wc -l`
+    echo $running_intances_count
     all_instance_count=`aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output text | wc -l`
 
     # Show all the instance we have
     echo "---- We have $all_instance_count instances, $running_intances_count running: "
-    aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output text
-    
+    aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output text | wc -l
+
     key=0
     while [ $key -ne 3 ]
     do
@@ -35,24 +44,26 @@ do
       echo "2. To see all our stopped instances"
       echo "3. To see all our Main Site instances"
       echo "4. To see all our DR instances "
-      echo "3. Go back to Menu"
+      echo "5. To see a specifiv Instance"
+      echo "6. Go back to Menu"
       read key
 
       case $key in
       1)
         echo "Running Instances:"
-        aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output text | grep running
+        cat /tmp/intances | grep running
         ;;
       2)
-        aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output text | grep stopped
+        cat /tmp/intances | grep stopped
         ;;
       3)
         
        clear
        ;;
       esac
-     
+#aws ec2 describe-instances --filter Name=tag:Name,Values=STG-PUBLIC-WEB-2 --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output text
     done
   esac
 done
-#aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output text
+#aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output texta
+#aws ec2 describe-instances --filter Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output text
