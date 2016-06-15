aea3b605bfb0a5db2706b7d1ba05a58591c06ec7
root <root@JenkinsTest1.(none)>
2016-06-15 03:58:12 -0400

    First Commit

---
+++ aws_mgmt.sh
@@ -0,0 +1,26 @@
+while :
+do
+  clear
+
+  # Menu
+  echo "----- Welcome to the AWS Condole Managment-----"
+  echo ""
+  echo "----- Choose one of this options to get current weather data by: "
+  echo "----- 1. Instances"
+  echo "----- 2. Volumes"
+  echo "----- 3. Load Balancers"
+  echo "----- 4. VPC's"
+  echo "----- 5. Exit"
+  # The choice
+  read -p "Insert the number you choose: " choice
+
+  # Checking all the cases
+  case $choice in
+  1)
+    ;;
+  esac
+
+done
+
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId, PrivateIpAddress, InstanceType, State.Name, Placement.AvailabilityZone, Tags[1].Value, Tags[0].Value]' --output text
+
